1399fb042036c820a127ab6ced3e3b1f5bdcd705
Yim yejin <loveim0112@naver.com>
2021-10-05 03:34:43 +0900

    Post 모델에 이미지와 파일 필드 추가하기

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
 # Create your views here.
-class PostList(ListView) :
+class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+
 #    template_name = 'blog/post_list.html'
 # post_list.html
 
-class PostDetail(DetailView) :
+class PostDetail(DetailView):
     model = Post
 #    template_name = 'blog/post_detail.html'
 # post_detail.html
 
-#def index(request):
+# def index(request):
 #    posts = Post.objects.all().order_by('-pk')
 #
 #    return render(request, 'blog/post_list.html',
@@ -26,7 +28,7 @@ class PostDetail(DetailView) :
 #                  )
 
 
-#def single_post_page(request, pk):
+# def single_post_page(request, pk):
 #    post = Post.objects.get(pk=pk)
 #
 #    return render(request, 'blog/post_detail.html',
