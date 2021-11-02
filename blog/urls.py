3d5d9fc521b46b83bd0698863b466438e5f38369
Yim yejin <loveim0112@naver.com>
2021-11-03 06:51:44 +0900

    포스트 목록/상세 페이지에 태그 기능 추가 및 태그페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 #    path('<int:pk>/', views.single_post_page), 상세페이지
 #    path('', views.index),
 # 현재는 CBV
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
