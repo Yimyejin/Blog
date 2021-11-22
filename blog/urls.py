92fe2dbb82542f64bfc4f83b053938aec8ebee43
Yim yejin <loveim0112@naver.com>
2021-11-22 23:04:37 +0900

    검색 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 #    path('<int:pk>/', views.single_post_page), 상세페이지
 #    path('', views.index),
 # 현재는 CBV
+    path('search/<str:q>/', views.PostSearch.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
